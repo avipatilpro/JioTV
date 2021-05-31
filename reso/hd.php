fb3a18b30834d504c8f126f5103edab54bf23fd9
Avishkar Patil <avishkarpatil29@gmail.com>
2021-05-31 19:09:00 +0530

    Initial Commit

--- reso/hd.php
+++ reso/hd.php
@@ -127,7 +127,7 @@ function videovisible() {
     document.getElementById('loading').style.display = 'none'
 }
 
-var url="m3u8.php?c=<?php echo $_REQUEST["c"]; ?>&q=800&e=junk.m3u8";
+var url="../m3u8.php?c=<?php echo $_REQUEST["c"]; ?>&q=800&e=junk.m3u8";
 
 plyr.setup(video);
 
