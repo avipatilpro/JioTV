f26e2a103b966ef15bad6200d852c7678e65bc6d
Avishkar Patil <avishkarpatil29@gmail.com>
2021-06-03 13:05:18 +0530

    Initial Commit

--- token.php
+++ token.php
@@ -9,7 +9,7 @@
 
 $jctBase = "cutibeau2ic"; # Channge To Your Token
 
-$ssoToken = "YOUR_SSOTOKEN"; #Change This YOUR_SSOTOKEN with your SSOTOKEN otherwise script will not work
+$ssoToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1bmlxdWUiOiI0NWU1OTMzMC1mZmQ4LTRjNmMtOTk4NS1mZTg5ZjIyMjMyZWIiLCJ1c2VyVHlwZSI6IlJJTHBlcnNvbiIsImF1dGhMZXZlbCI6IjQwIiwiZGV2aWNlSWQiOiJjNDMyMTNhOGMzYjljNTAxYzI4MDY1NjRiYzNkNjlhNDkwY2IwZDdiOTM0MzI4MDQ5Mzk1MTU0Mzk3NmViOTM2ZDcxNmZmZTMwYmNjM2UxOWU1MTMzZjNiNTg1MjNiNGY3OGU0YWMxYjFhYzc4MTVjNTNjMTE5MDBkYTM0ZTEwZCIsImp0aSI6IjZiMDQwNDdlLTk4OTgtNDkzOC04M2ZjLTJmMGNiMjBjYzkwYSIsImlhdCI6MTYyMjEyNDkzNn0.VGU9LzWWshG2RAKm6jnAG0CUil7NvkZlWa04jJR8koU"; #Change This YOUR_SSOTOKEN with your SSOTOKEN otherwise script will not work
 function tokformat($str)
 {
 $str= base64_encode(md5($str,true));
