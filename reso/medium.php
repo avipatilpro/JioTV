7815130832072afc23394a33736e9509187d9107
Avishkar Patil <avishkarpatil29@gmail.com>
2021-06-03 17:19:37 +0530

    Initial Commit

--- reso/medium.php
+++ reso/medium.php
@@ -124,7 +124,7 @@ html {
 </div>
 
 <div id="player">
-<video id="video" style="width:100%;height:100%;object-fit: fill;"></video>
+<video id="video" style="width:100%;height:100%;"></video>
 </div>
 <script>
   setTimeout(videovisible, 4000)
