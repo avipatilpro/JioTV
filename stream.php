7a6c18b2be8c7cba4bbbdb3a53a9c47f88535aa6
project@a <80537197+project-a-app@users.noreply.github.com>
2021-08-26 09:00:52 +0600

    Update stream.php

--- stream.php
+++ stream.php
@@ -1,5 +1,5 @@
 <?php
-$p= @file_get_contents("http://localhost/token.php"); # Change This Path According to Your Server and Folder
+$p= @file_get_contents("http://projectjio/joxxx.php"); # Change This Path According to Your Server and Folder
 
 
 # © 2021 Avishkar Patil DO NOT EDIT ANYTHING TO KEEP RUNNING
