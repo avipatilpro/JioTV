e9f4d869277da2eb743ebf727f52b75ee421591c
Avishkar Patil <avishkarpatil29@gmail.com>
2021-11-13 20:53:09 +0530

    Initial Commit

--- autoq.php
+++ autoq.php
@@ -2,6 +2,8 @@
 header("Content-Type: application/vnd.apple.mpegurl");
 echo '#EXTM3U'.PHP_EOL;
 echo '#EXT-X-VERSION:3'.PHP_EOL;
+echo '#EXT-X-STREAM-INF:PROGRAM-ID=1,CLOSED-CAPTIONS=NONE,BANDWIDTH=250000,RESOLUTION=426x240'.PHP_EOL;
+echo 'http://localhost/live.php?c='.$_GET['c'].'&q=250&e=.m3u8'.PHP_EOL;
 echo '#EXT-X-STREAM-INF:PROGRAM-ID=1,CLOSED-CAPTIONS=NONE,BANDWIDTH=400000,RESOLUTION=854x480'.PHP_EOL;
 echo 'http://localhost/live.php?c='.$_GET['c'].'&q=400&e=.m3u8'.PHP_EOL;
 echo '#EXT-X-STREAM-INF:PROGRAM-ID=1,CLOSED-CAPTIONS=NONE,BANDWIDTH=600000,RESOLUTION=1024x576'.PHP_EOL;
@@ -11,5 +13,7 @@ echo 'http://localhost/live.php?c='.$_GET['c'].'&q=800&e=.m3u8'.PHP_EOL;
 echo '#EXT-X-STREAM-INF:PROGRAM-ID=1,CLOSED-CAPTIONS=NONE,BANDWIDTH=1200000,RESOLUTION=1920x1080'.PHP_EOL;
 echo 'http://localhost/live.php?c='.$_GET['c'].'&q=1200&e=.m3u8'.PHP_EOL;
 
+
+// Change The Path Localhost To Your Server According To You 
 
 ?>
